b5d96cc8ced0f15e5b8e6dac9f76a3d2249f2810
Thomas Julou <thomas.julou@gmail.com>
2022-12-12 17:40:56 +0100

    Adding data files stored in `./data` (using LFS).

---
+++ data/20180703_ASC662_M9gly04pc_TMG/20180703_ASC662_M9gly04pc_TMG.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:15ae114da67b0b177d528ff87891b85ab85f561a0f988d6bb3062120bff3e5a8
+size 729
---
+++ data/sugarMix-cellWidth/sugarMix-cellWidth-fluoMask.ijm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c1e3dcbb52df24be1f6dbe417a4b35b2b8a8cb818ce595203942ab300034469
+size 635
